af4a9b7d42a08a2bfc1c7e32474c6526b984d518
Ksenx73 <hello@itcampe.com>
2021-10-19 12:17:57 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,13 @@
+package it.campe;
+
+public class Main {
+
+    public static String secret = "PasSwoRd";
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("доступ разрешен");
+        //напишите тут ваш код
+        System.out.println("доступ запрещен");
+    }
+}
